ab85a787fc2598c3dea387ad4efd4d497f096db6
Robertas K <rethwall@gmail.com>
2023-11-09 10:25:26 +0200

    Roberto commitas
    New file added

---
+++ mystery_module_RK.py
@@ -0,0 +1,8 @@
+# Pabaik mano minti
+
+u_input = input('Give me a sentence, and I will tell you the length of its first and last component.')
+
+new_list = u_input.split()
+for comp in new_list:
+
+print(f'The length of the first component is {}, while of the the last is {}')

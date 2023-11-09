214c0f78103d29bc5fd9fbc11c0d843c6fca512c
Robertas K <rethwall@gmail.com>
2023-11-09 10:56:47 +0200

    Eksperimentai su dict kurimu
    Panaudojau destruktyvu metoda ir dict comprehensions, bet taip galima ir su zip() padaryt.

--- rolando_prg.py
+++ rolando_prg.py
@@ -9,3 +9,21 @@ print(bendras)
 # Spausdinam su pridetu tekstu
 for v in bendras:
     print(v[0], v[1])
+# Sukuriu zodyna is dvieju listu, destruktyvu
+print('-' * 20)
+skoniu_dict = {}
+for fruit in listas:
+    for taste in pazym:
+        skoniu_dict[fruit] = taste
+        pazym.remove(taste)
+        break
+# Printuoju dict:
+for key, value in skoniu_dict.items():
+    print(key,'->',value)
+# Lengvas metodas su dict comprehension
+listas = ["Kriaušė", "Obuolys", "Slyva", "Serbentas"]
+pazym = ["skanu", "skanu", "neskanu", "neskanu"]
+print('-' * 20)
+skoniu_dict2 = {listas[i]: pazym[i] for i in range(len(pazym))}
+for key, value in skoniu_dict2.items():
+    print(key,'->',value)
